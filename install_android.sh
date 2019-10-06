156116486b14fc7e15d71e503de822734cb5fb4b
Bronson <bronsonmathews@gmail.com>
2019-10-06 17:58:02 +1030

    work and android cmake

--- install_android.sh
+++ install_android.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
+
+# https://github.com/LaurentGomila/qt-android-cmake
+# https://github.com/calincru/QML-Android-Demo
+
 # ensure the following is installed
 # android-sdk
 # android-ndk
 
 # deinfe temorary paths if need be
-#export ANDROID_NDK="$PATH:test"
-#export ANDROID_SDK="$PATH:test"
-#export JAVA_HOME="$PATH:test"
+export ANDROID_NDK="$PATH:/opt/android-ndk/"
+export ANDROID_SDK="$PATH:/opt/android-sdk/"
+export JAVA_HOME="$PATH:/usr/lib/jvm/default/"
+#export ANDROID_TOOLCHAIN_ROOT="$PATH:/opt/android-ndk/toolchains/"
 
 mkdir -p build_android
 cd build_android
-cmake -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK}/build/cmake/android.toolchain.cmake .
-cmake ..
+cmake -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK}/build/cmake/android.toolchain.cmake -DANDROID=ON ..
+#cmake ..
 make
 #sudo make install
 $SHELL
